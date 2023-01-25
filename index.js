45c0219464d954291a926716a3ea1261a78fab88
Yevhenii Chernenko <chernenko.yevheniy@gmail.com>
2023-01-25 06:50:33 +0200

    added blogRouter

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config.js')
 const Blog = require('./models/blog.js')
+const blogRouter = require('./controllers/blogs.js')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -12,23 +13,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
